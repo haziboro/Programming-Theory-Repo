7a4ecfed28aedcb327f6a112907adcd54698c821
John Stevens <62948831+haziboro@users.noreply.github.com>
2021-07-29 22:50:42 -0400

    Camera control
    Added horizontal camera control

---
+++ ProgrammingTheoryProject/Assets/Scripts/CameraControl.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraControl : MonoBehaviour
+{
+    public float rotationSpeed = 20.0f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        float horizontalInput = Input.GetAxis("Horizontal");
+        transform.Rotate(Vector3.up, horizontalInput * rotationSpeed * Time.deltaTime);
+    }
+}
